705167f4a2f9f6cbb1cbcd8dc3c28c562f7f4af4
nhuq001 <31972361+nhuq001@users.noreply.github.com>
2017-12-06 17:34:41 -0500

    Creating the rate matrix
    Creating the rate matrix for each element

--- rate_matrix.cu
+++ rate_matrix.cu
@@ -1,4 +1,4 @@
-__global__ void rate_matrix(int *config, int *rate, species *all)//species is a struct that holds reaction and diffusion rates of a species
+__global__ void rate_matrix_1(int *config, int *rate, species *all)//species is a struct that holds reaction and diffusion rates of a species
 {
 	//element will be used for rate[index]
 	int element = blockIdx.x*blockDim.x + threadIdx.x; //index for this instance based on which core and thread is running
@@ -7,6 +7,15 @@ __global__ void rate_matrix(int *config, int *rate, species *all)//species is a 
 		rate[element] = sum_reactions(element, config, all); //sums reaction rate for subvolume x. Don't know how to find x yet in config
 	if(element % 3 == 2) //second column
 		rate[element] = sum_diffusions(element, config, all); //sums diffusion rate for subvolume x. Don't know how to find x yet in config
+	if(element % 3 == 0) //third column
+		rate[element] = rate[element - 1] + rate[element - 2]; //sums reaction and diffusion rate for subvolume x.
+}
+__global__ void rate_matrix_2(int *config, int *rate, species *all)//species is a struct that holds reaction and diffusion rates of a species
+{
+	//element will be used for rate[index]
+	int element = blockIdx.x*blockDim.x + threadIdx.x; //index for this instance based on which core and thread is running
+													   // blockDim.x is the total amount of threads
+	
 	if(element % 3 == 0) //third column
 		rate[element] = rate[element - 1] + rate[element - 2]; //sums reaction and diffusion rate for subvolume x.
 }
